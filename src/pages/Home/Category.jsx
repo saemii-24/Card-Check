013286852c2d5f65790c19fbab84eaa452d8f8f4
saemii-24 <hims2419@gmail.com>
2023-10-05 09:10:54 +0900

    before animation

--- src/pages/Home/Category.jsx
+++ src/pages/Home/Category.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Category = () => {
   return (
     <>
-      <Container></Container>
+      <Container>
+        <Row>
+          <Col>1</Col>
+          <Col>2</Col>
+          <Col>3</Col>
+        </Row>
+      </Container>
     </>
   )
 }
--- src/pages/Home/Main.jsx
+++ src/pages/Home/Main.jsx
@@ -7,8 +7,8 @@ const Main = () => {
     <>
       <Container fluid className="main">
         <Row>
-          <Col className="right col-sm-6">1 of 2</Col>
-          <Col className="left col-sm-6">2 of 2</Col>
+          <Col className="right col-lg-5">1 of 2</Col>
+          <Col className="left col-lg-7">2 of 2</Col>
         </Row>
       </Container>
     </>
