9d4736cb77a1322579ada106e38cd1d856e865d0
saemii-24 <hims2419@gmail.com>
2023-10-14 14:15:21 +0900

    2023.10.14

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import CardBox from './components/CardBox'
 import CardSearch from './pages/CardSearch/CardSearch'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import ScrollToTop from './scrollToTop'
 function App() {
   return (
     <>
       <Header />
       <CardBox />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cardSearch" element={<CardSearch />} />
